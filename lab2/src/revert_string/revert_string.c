f295dd1be355b9e9a89e1c09c9f39aefea673e2e
Pavel9Kor <73028410+Pavel9Kor@users.noreply.github.com>
2021-06-05 17:18:24 +0300

    Add files via upload

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -2,6 +2,24 @@
 
 void RevertString(char *str)
 {
-	// your code here
+	   int i = 0;
+     int end;
+     char buff;
+
+     while(str[i] != '\0')
+     {
+         i++;
+     }
+
+     end = i - 1;
+     i = 0;
+
+     while(i < end/2 + 1)
+     {
+         buff = str[i];
+         str[i] = str[end - i];
+         str[end - i] = buff;
+         i++;
+     }
 }
 
